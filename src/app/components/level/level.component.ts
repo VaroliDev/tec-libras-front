d7ad6e19c18445b9a05265ec0b11a1c6419909d4
Helena Neves <helena.s.neves14@gmail.com>
2025-08-22 08:46:38 -0300

    add modular style

--- src/app/components/level/level.component.ts
+++ src/app/components/level/level.component.ts
@@ -8,7 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class LevelComponent implements OnInit {
   @Input() level: number = 1;     // Número do nível
-  @Input() percent: number = 80;   // Progresso em %
+  @Input() percent: number = 70;   // Progresso em %
+  @Input() levelstyle: number = this.level % 2;
 
   radius = 65;          // raio do círculo
   circumference = 2 * Math.PI * this.radius;
