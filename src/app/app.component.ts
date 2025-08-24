7b5efe25f23423be696af4332f5dec879c33aaf0
Samuel <samuel.varoli@etec.sp.gov.br>
2025-08-24 09:43:12 -0300

    add conta loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-  import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './services/theme.service';
 import { UserService } from './services/user.service';
@@ -35,9 +35,10 @@ export class AppComponent {
   
     const token = JSON.parse(tokenData);
 
-    //redireciona caso esteja logado -NAO ESTA FUNCIONANDO (nseipq)-
-    if(token.token && this.router.url == '/login' || this.router.url == '/cadastro' || this.router.url == '/'){
-      this.router.navigate(['/inicio'])
+    if(token.token){
+      if(this.router.url === '/login' || this.router.url === '/cadastro'){
+        this.router.navigate(['/inicio'])
+      }
     }
 
     this.userService.renewData(token.token).subscribe(
