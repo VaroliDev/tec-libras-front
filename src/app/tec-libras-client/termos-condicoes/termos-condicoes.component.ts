4a4d298e49fb89b777b3238d61508a8c548a5740
Helena Neves <helena.s.neves14@gmail.com>
2025-08-23 21:53:53 -0300

    feat: create component termos-condicoes

---
+++ src/app/tec-libras-client/termos-condicoes/termos-condicoes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termos-condicoes',
+  imports: [],
+  templateUrl: './termos-condicoes.component.html',
+  styleUrl: './termos-condicoes.component.scss'
+})
+export class TermosCondicoesComponent {
+
+}
