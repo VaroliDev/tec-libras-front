05218def63d83032fb9cf51464ef1f03f99bde51
Samuel <samuel.varoli@etec.sp.gov.br>
2025-08-29 14:29:53 -0300

    fix: environments initialization

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { API_URL } from '../../environments/environment.development';
+import { API_URL } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
