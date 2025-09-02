6988425df6abe8fc5055b9900efe454b7ebb1312
Helena Neves <helena.s.neves14@gmail.com>
2025-09-02 15:39:12 -0300

    fix: name nivel to level-theme

---
+++ src/app/tec-libras-client/nivel/level-themes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nivel',
+  imports: [],
+  templateUrl: './level-themes.component.html',
+  styleUrl: './level-themes.component.scss'
+})
+export class LevelThemesComponent {
+
+}
