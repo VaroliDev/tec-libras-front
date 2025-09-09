d44a130170e1274de180bbd8f38cf147a437fcf4
Helena Neves <helena.s.neves14@gmail.com>
2025-09-08 23:12:57 -0300

    feat: add themes page html, css, ts

--- src/app/tec-libras-client/level-themes-subjects/themes/themes.component.ts
+++ src/app/tec-libras-client/level-themes-subjects/themes/themes.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../components/footer/footer.component";
+import { HeaderComponent } from "../../../components/header/header.component";
 
 @Component({
   selector: 'app-themes',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent,],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.scss'
 })
