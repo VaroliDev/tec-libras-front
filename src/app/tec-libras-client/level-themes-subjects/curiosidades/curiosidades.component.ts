3ddd51cbca3970ad035c48b02fea59cbb0224cae
Helena Neves <helena.s.neves14@gmail.com>
2025-09-09 09:32:29 -0300

    feat create curiosidades page

--- src/app/tec-libras-client/level-themes-subjects/curiosidades/curiosidades.component.ts
+++ src/app/tec-libras-client/level-themes-subjects/curiosidades/curiosidades.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../components/header/header.component";
 import { FooterComponent } from "../../../components/footer/footer.component";
+import { LisiBalloonComponent } from '../../../components/lisi-balloon/lisi-balloon.component';
 
 @Component({
   selector: 'app-curiosidades',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, LisiBalloonComponent],
   templateUrl: './curiosidades.component.html',
-  styleUrl: './curiosidades.component.scss'
+  styleUrl: './curiosidades.component.scss',
+  standalone: true
 })
 export class CuriosidadesComponent {
 
