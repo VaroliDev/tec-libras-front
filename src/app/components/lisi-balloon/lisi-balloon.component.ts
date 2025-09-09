eac34181519f8b47aef829edeba3e640387a461e
Helena Neves <helena.s.neves14@gmail.com>
2025-09-09 09:31:24 -0300

    feat create component lisi balloon

---
+++ src/app/components/lisi-balloon/lisi-balloon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lisi-balloon',
+  imports: [],
+  templateUrl: './lisi-balloon.component.html',
+  styleUrl: './lisi-balloon.component.scss'
+})
+export class LisiBalloonComponent {
+
+}
