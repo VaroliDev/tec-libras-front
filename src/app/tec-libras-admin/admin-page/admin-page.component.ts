76beef1c5ff3446fd23230c7e0f9c47f213c17b5
VaroliDev <scvaroli@outlook.com>
2025-09-12 10:44:46 -0300

    add: verification for login page

--- src/app/tec-libras-admin/admin-page/admin-page.component.ts
+++ src/app/tec-libras-admin/admin-page/admin-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-admin-page',
@@ -10,13 +11,35 @@ import { Router } from '@angular/router';
   styleUrl: './admin-page.component.scss'
 })
 export class AdminPageComponent {
-  router = inject(Router);
-
+  private router = inject(Router);
+  private userService = inject(UserService);
   pagLista(){
     this.router.navigate(['/lista-usuarios']);
   }
 
   pagCadastroConteudo(){
     this.router.navigate(['/conteudo'])
+  }
+  ngOnInit(): void{
+  
+  const userDataString = localStorage.getItem('token') || '';
+  if(!userDataString){
+    this.router.navigate(['/login']);
+    return;
+  }
+
+  const userData = JSON.parse(userDataString);
+
+  this.userService.getUserRole(userData.id).subscribe({
+      next: (data) => {
+        if(data.role !== 'admin'){
+          this.router.navigate(['/inicio']);
+        }
+      },
+      error: (err) => {
+        console.error('Erro ao buscar papel do usuário:', err);
+        return
+      }
+    });
   }
 }
