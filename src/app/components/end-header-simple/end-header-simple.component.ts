1317a8f45d15d534e5cbf85e8891db3da219f9b9
Helena Neves <helena.s.neves14@gmail.com>
2025-11-28 01:23:59 -0300

    create component end-header-simple

---
+++ src/app/components/end-header-simple/end-header-simple.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-header-simple',
+  imports: [],
+  templateUrl: './end-header-simple.component.html',
+  styleUrl: './end-header-simple.component.scss'
+})
+export class EndHeaderSimpleComponent {
+
+}
