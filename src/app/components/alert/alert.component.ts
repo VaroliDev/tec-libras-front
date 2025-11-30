db09b89aa610479eb56ee63106e240e2d21ce89c
Helena Neves <helena.s.neves14@gmail.com>
2025-11-30 01:52:10 -0300

    feat alert

---
+++ src/app/components/alert/alert.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- IMPORTAR
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [CommonModule],  // <-- ADICIONAR AQUI
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
+})
+export class AlertComponent {
+  @Input() type: 'success' | 'error' | 'warning' = 'success';
+  @Input() message: string = '';
+
+  show: boolean = false;
+
+  close() { this.show = false; }
+
+  open(message: string, type: 'success' | 'error' | 'warning' = 'success') {
+    this.message = message;
+    this.type = type;
+    this.show = true;
+    setTimeout(() => this.close(), 3000);
+  }
+}
