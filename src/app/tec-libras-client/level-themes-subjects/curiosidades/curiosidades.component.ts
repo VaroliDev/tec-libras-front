fa181c9c9ef95cf8d1cb165b20cea54186724b82
Samuel <samuel.varoli@etec.sp.gov.br>
2025-12-01 18:50:34 -0300

    fix: multiple curiosidades now can be displayed

--- src/app/tec-libras-client/level-themes-subjects/curiosidades/curiosidades.component.ts
+++ src/app/tec-libras-client/level-themes-subjects/curiosidades/curiosidades.component.ts
@@ -20,32 +20,56 @@ export class CuriosidadesComponent {
   private levelService = inject(LevelService);
 
   protected theme = this.levelService.getTheme();
-  protected curiosidade: string = '';
+  protected curiosidades: string[] = [];
+  protected curiosidadeAtual: string = '';
+  protected indiceAtual: number = 0;
 
-  protected finished: boolean = false
-  protected redict: string = 'temas'
+  protected finished: boolean = false;
+  protected redict: string = 'temas';
 
   @ViewChild('alert') alert!: LisiFeedbackComponent;
 
-
-
   PagInicio(){
-    this.router.navigate(['temas'])
+    this.router.navigate(['temas']);
   }
 
   PagBack(){
-    this.router.navigate(['temas'])
+    if (this.indiceAtual > 0) {
+      this.indiceAtual--;
+      this.curiosidadeAtual = this.curiosidades[this.indiceAtual];
+    } else {
+      this.router.navigate(['temas']);
+    }
   }
 
   PagNext(){
-    this.levelService.registerProgress(5);
-    this.alert.open('Você é demais!', 'Parabéns, você concluiu o último tópico! Pronto(a) para iniciar uma nova fase?', 'success');
-
+    if (this.indiceAtual < this.curiosidades.length - 1) {
+      this.indiceAtual++;
+      this.curiosidadeAtual = this.curiosidades[this.indiceAtual];
+    } else {
+      this.levelService.registerProgress(5);
+      this.alert.open('Você é demais!', 'Parabéns, você concluiu o último tópico! Pronto(a) para iniciar uma nova fase?', 'success');
+    }
   }
 
   async ngOnInit(){
     const data = await this.levelService.getThemeData(this.theme as number);
-    this.curiosidade = data.curiosidade;
-    console.log(this.curiosidade);
+    
+    this.curiosidades = data.curiosidade
+      .split('<hr/>')
+      .map((c: string) => c.trim())
+      .filter((c: string) => c.length > 0);
+    
+    this.curiosidadeAtual = this.curiosidades[0];
+    
+    console.log('Total de curiosidades:', this.curiosidades.length);
+    console.log('Curiosidades:', this.curiosidades);
+  }
+
+  get isUltimaCuriosidade(): boolean {
+    return this.indiceAtual === this.curiosidades.length - 1;
+  }
+  get isPrimeiraCuriosidade(): boolean {
+    return this.indiceAtual === 0;
   }
 }
